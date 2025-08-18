e14f0514e6796945815dc2f4d1c0b28903ff33e0
shahrad <shahradArianNezhad@gmail.com>
2025-08-18 19:25:57 +0330

    added content lenght parsing and readed app for multithreaded downloads

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <pthread.h>
 #include "../include/ip.h"
 #define PORT 80
 
@@ -18,9 +19,6 @@ int main(int argc,char** argv){
     }
 
 
-
-
-
     char res[2048];
     char request[256];
     struct sockaddr_in server_addr;
@@ -30,6 +28,7 @@ int main(int argc,char** argv){
     "GET %s HTTP/1.1\r\n"
     "Host: %s\r\n"
     "Connection:close\r\n"
+    // "Range: bytes=0-60\r\n"
     "\r\n",
     argv[2],argv[1]);
 
@@ -50,35 +49,39 @@ int main(int argc,char** argv){
         return -2;
     }
 
+
     int fd = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
 
-
-
-
-    // printf("%s",request);
     if(send(socketfd,request,request_len,0)<0){
         printf("send failed\n");
         return -3;
     }
 
     int bytes_recv;
-    int hit_header=0;
     char* temp;
-    while((bytes_recv=recv(socketfd,res,2048,0))>0){
-        if(hit_header==1){
-            write(fd,res,bytes_recv);
-            continue;
-        }
-        if((temp = strstr(res,"\r\n\r\n"))!=NULL){
-            hit_header=1;
-            int header_size = temp-res+4;
-            write(fd,temp,bytes_recv-header_size);
-            // printf("%s\n",temp);
-            continue;
-        }
+    bytes_recv=recv(socketfd,res,2048,0);
+    if((temp = strstr(res,"\r\n\r\n"))!=NULL){
+        int header_size = temp-res+4;
+        write(fd,temp,bytes_recv-header_size);
+        char* cont_len_ptr = strstr(res,"content-length:");
 
+        // skip the 'content-lenght:'
+        cont_len_ptr+=16;
+
+        // content lenght in bytes
+        char cont_len[15];
+        int int_cont_len;
+
+        for(int i=0;i<50;i++){
+            if((*(cont_len_ptr+i))=='\r'&&(*(cont_len_ptr+i+1))=='\n'){
+                break;
+            }
+            cont_len[i]=*(cont_len_ptr+i);
+        }
+        sscanf(cont_len,"%d",&int_cont_len);
+        printf("%d\n",int_cont_len);
+        
     }
-
     return 0;
 }
